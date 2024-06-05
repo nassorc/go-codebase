63381d8f548a10ea53fa03fa6a738a1956a52c2d
nassorc <macrossan17@gmail.com>
2024-06-04 22:04:20 -0700

    update

---
+++ examples/platformer/go.mod
@@ -0,0 +1,3 @@
+module gandalf/examples/platformer
+
+go 1.22
--- main.go
+++ main.go
@@ -2,325 +2,247 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"reflect"
-	"slices"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-const MAX_SIGNATURE = 16
+var MAX_SIGNATURE_SIZE = 16
 
-func makeWorldComponent[T any](w *world) *component[T] {
-	component := &component[T]{
-		world: w,
-	}
+func UNUSED(name ...interface{}) {}
 
-	t := reflect.TypeOf(component)
-	elem := reflect.TypeOf((*T)(nil)).Elem()
-
-	component.t = t
-	component.elem = elem
-	component.id = w.registerComponent(component)
-
-	return component
+type CTest struct {
+  val int
 }
 
-func makeWorldSystem[T *ISystem](w *world) *system[T] {
-	system := &system[T]{
-		world: w,
-	}
-
-  // var obj T
-	// w.registerSystem(obj)
-  // t := reflect.TypeOf(&obj)
-  // zero := reflect.Zero(t)
-  // fmt.Println("t", t, zero, obj)
-
-	// z := reflect.Zero(reflect.TypeOf((*T)(nil))).Interface().(*ISystem)
- //  fmt.Println("checking", reflect.ValueOf((*T)(nil)))
-	//
- //  // fmt.Println("system", reflect.Zero() )
-	// zero := reflect.Zero(reflect.TypeOf((*T)(nil)).Elem()).Interface().(ISystem)
-	// zero2 := reflect.Zero(reflect.TypeOf((*T)(nil))).Interface().(ISystem)
- //  fmt.Println("zero", zero)
- //  fmt.Println("zero2", reflect.TypeOf(zero2))
-	// // zero := reflect.Zero(reflect.TypeOf((*T)(nil)).Elem().Elem()).Interface().(ISystem)
-	// w.registerSystem(zero2)
-
-	return system
+type CComp struct {
+  val int
 }
 
-func newWorld(entityCapacity int) *world {
-	var rb *ringbuffer[Entity] = NewRingBuffer[Entity](entityCapacity)
-
-	for idx := Entity(0); idx < Entity(entityCapacity); idx++ {
-		Enqueue(rb, idx)
-	}
-
-	return &world{
-		// Component_______________________________________________________________
-		componentMap:        make(map[int]*componentArray),
-		componentToId:       make(map[reflect.Type]int),
-		elemToComponent:     make(map[reflect.Type]reflect.Type),
-		// Entity__________________________________________________________________
-		availEntities:       rb,
-		entityToSignature:   make(map[Entity]*Signature),
-		// System__________________________________________________________________
-		systems:             make([]ISystem, 0, 0),
-		systemElemToIdx:     make(map[reflect.Type]int),
-    systemIdToSignature: make(map[int]*Signature),
-	}
+type PlayerSystem struct {
+  entities []Entity
 }
 
-func newComponentArray(t reflect.Type) *componentArray {
-	return &componentArray{
-		data:         reflect.MakeSlice(reflect.SliceOf(t), 0, 0),
-    entityToData: make(map[uint32]int),
-    dataToEntity: make(map[int]uint32),
-	}
+func (s *PlayerSystem) update(w *World) {}
+func (s *PlayerSystem) render(w *World) {}
+func (s *PlayerSystem) addEntity(entity Entity) {
+  s.entities = append(s.entities, entity)
 }
 
-type Entity = uint32
+type IScene interface{
+  setup()
+}
+
+type ComponentArr struct {
+  id           int            // position in the world.components array
+  data         reflect.Value
+  entityToData map[Entity]int
+}
+
+func (c *ComponentArr) Set(entity Entity, value reflect.Value) {
+  var idx = c.data.Len()
+  c.data = reflect.Append(c.data, value)
+  c.entityToData[entity] = idx
+}
+
+func (c *ComponentArr) Get(entity Entity) reflect.Value {
+  var idx = c.entityToData[entity]
+  return c.data.Index(idx)
+}
+
+type Entity = int
+
+func NewWorld() *World {
+  availEntities := NewRingBuffer[Entity](10)
+
+  for idx := 0; idx < 10; idx++ {
+    Enqueue(availEntities, idx)
+  }
+
+  return &World{
+    componentToId: make(map[reflect.Type]int),
+    availEntities: availEntities,
+    entityToSignature: make(map[Entity]*Signature),
+  }
+}
+
+type World struct {
+
+  systems           []ISystem
+  systemSignatures  []*Signature 
+
+  components        []*ComponentArr
+  componentToId     map[reflect.Type]int  // maps component element type to its position in the world.components array
+
+  availEntities     *ringbuffer[Entity]
+  entityToSignature map[Entity]*Signature
+}
 
 type ISystem interface {
-	Update(w *world)
-  Id() int
-  AddEntity(entity Entity)
+  update(*World)
+  render(*World)
+  addEntity(Entity)
 }
 
-type system[T any] struct {
-	world *world
-  id    int
+func (w *World) registerSystem(system ISystem, signature *Signature) {
+  w.systems = append(w.systems, system)
+  w.systemSignatures = append(w.systemSignatures, signature)
 }
 
-func (s *system[T]) Id() int {
-  return s.id
+func (w *World) registerComponent(components ...interface{}) {
+  for _, component := range components {
+    var cType = reflect.TypeOf(component)
+    var id = len(w.componentToId)
+    var componentArr = ComponentArr {
+      id: id,
+      data: reflect.MakeSlice(reflect.SliceOf(cType), 0, 0),
+      entityToData: make(map[Entity]int),
+    }
+
+    w.components = append(w.components, &componentArr) 
+    w.componentToId[cType] = id
+  }
 }
 
-type IComponent interface {
-	Id() int
-	Type() reflect.Type
-	ElemType() reflect.Type
+func (w *World) getComponent(entity Entity, component interface{}) {
+  // fmt.Println(world.components[0].data.Index(0))
+  t := reflect.TypeOf(component).Elem()
+  value := reflect.ValueOf(component)
+  fmt.Println("getting component", t.Elem())
+  var componentArr = w.components[w.componentToId[t]]
+  var idx = componentArr.entityToData[entity]
+  // value.Set(componentArr.data.Index(idx))
+  value.Elem().Set(componentArr.data.Index(idx))
+  // fmt.Println(value, value)
 }
 
-type component[T any] struct {
-	world *world
-	id    int
-	t     reflect.Type
-	elem  reflect.Type
+func (w *World) getComponentId(component interface{}) int {
+    var cType = reflect.TypeOf(component)
+    id, ok := w.componentToId[cType]
+
+    if !ok {
+      panic(fmt.Errorf("getComponentId: %v is not a component array", cType))
+    }
+
+    return id
 }
 
-func (c component[T]) Id() int {
-	return c.id
-}
+func (w *World) newEntity(components ...interface{}) Entity {
+  entity, err := Dequeue(w.availEntities)
+  if err != nil {
+    panic(err)
+  }
 
-func (c component[T]) Type() reflect.Type {
-	return c.t
-}
+  var eSignature = NewSignature(MAX_SIGNATURE_SIZE)
 
-func (c component[T]) ElemType() reflect.Type {
-	// elem := reflect.TypeOf((*T)(nil)).Elem()
-	return c.elem
-}
+  for _, component := range components {
+    var id = w.getComponentId(component)
+    var cValue = reflect.ValueOf(component)
 
-func (c *component[T]) set(entity Entity, val *T) {}
-func (c *component[T]) get(entity Entity)         {}
+    w.components[id].Set(entity, cValue)
+    eSignature.Set(id)
+  }
 
-type componentArray struct {
-	data         reflect.Value
-	entityToData map[Entity]int
-	dataToEntity map[int]Entity
-}
+  w.entityToSignature[entity] = eSignature
 
-type world struct {
-	// Components________________________________________________________________
-	componentMap    map[int]*componentArray // CHANGE TO ARRAY
-	componentToId   map[reflect.Type]int
-	elemToComponent map[reflect.Type]reflect.Type
-
-	// Entity____________________________________________________________________
-	availEntities     *ringbuffer[Entity]
-	entityToSignature map[Entity]*Signature
-
-	// System____________________________________________________________________
-	systems             []ISystem
-	systemElemToIdx     map[reflect.Type]int
-	systemIdToSignature map[int]*Signature
-}
-
-func UNUSED(a ...interface{}) {}
-
-func (w *world) registerSystem(system ISystem) int {
-	sType := reflect.TypeOf(system)
-	idx := len(w.systems)
-
-	w.systems = append(w.systems, system)
-	w.systemElemToIdx[sType] = idx
-
-	return idx
-}
-
-func (w *world) setSystemSignature(id int, signature *Signature) {
-  w.systemIdToSignature[id] = signature
-}
-
-func (w *world) registerComponent(component IComponent) int {
-	cId := len(w.componentMap)
-	cType := component.Type()
-	cElmType := component.ElemType()
-
-	// unnecessary?
-	w.componentMap[cId] = newComponentArray(cElmType)
-	w.componentToId[cType] = cId
-	w.elemToComponent[cElmType] = cType
-
-	return cId
-}
-
-func (w *world) getComponentId(component interface{}) int {
-	cElemType := reflect.TypeOf(component)
-
-	return w.componentToId[w.elemToComponent[cElemType]]
-}
-
-func (w *world) newEntity(components ...interface{}) Entity {
-	entity, _ := Dequeue(w.availEntities)
-
-	eSignature := NewSignature(MAX_SIGNATURE)
-
-	for _, component := range components {
-		cType := reflect.TypeOf(component)
-    UNUSED(cType)
-
-		cId := w.getComponentId(component)
-    cValue := reflect.ValueOf(component)
-
-
-		eSignature.Set(cId)
-
-    idx := w.componentMap[cId].data.Len()
-    w.componentMap[cId].data = reflect.Append(w.componentMap[cId].data, cValue)
-    w.componentMap[cId].dataToEntity[idx] = entity
-    w.componentMap[cId].entityToData[entity] = idx
-	}
-
-	w.entityToSignature[entity] = eSignature
-
-  // add entity to system 
-  for _, system := range w.systems {
-    sType := reflect.TypeOf(system)
-    id := w.systemElemToIdx[sType]
-
-    sSignature := w.systemIdToSignature[id]
+  // inform systems of new entity
+  for idx, system := range w.systems {
+    var sSignature = w.systemSignatures[idx]
 
     if (eSignature.Int() & sSignature.Int()) == sSignature.Int() {
-      system.AddEntity(entity)
+      system.addEntity(entity)
     }
   }
 
-	return entity
+  return entity
 }
 
-func (w *world) run() {
+func (w *World) update() {
   for _, system := range w.systems {
-    system.Update(w)
+    system.update(w)
   }
 }
 
-type Pos struct {
-	X int
-	Y int
-}
-
-type Transform struct {
-  pos rl.Vector2
-  vel rl.Vector2
-}
-
-type Player struct {}
-
-type PlayerSystem struct {
-  id       int
-	entities []Entity
-}
-
-
-func (s PlayerSystem) Update(w *world) {
-  for _, entity := range s.entities {
-    UNUSED(entity)
+func (w *World) render() {
+  for _, system := range w.systems {
+    system.render(w)
   }
 }
 
-func (s PlayerSystem) Id() int {
-  return s.id
+type Game struct {}
+
+func (g *Game) setup() {
+  world := NewWorld()
+  world.registerComponent(CTest{})
 }
 
-func (s *PlayerSystem) AddEntity(entity Entity) {
-  if (!slices.Contains(s.entities, entity)) {
-    s.entities = append(s.entities, entity)
-  }
-}
+type Engine struct{} 
 
-func getComponent[T any](w *world, entity Entity) *T {
-  elem := reflect.TypeOf((*T)(nil)).Elem()
-  t := w.elemToComponent[elem]
-  // fmt.Println(w.elemToComponent[t])
-  val, ok := w.componentToId[t]
-  fmt.Println("component id/idx?", val, ok)
-
-  idx := w.componentMap[val].entityToData[entity]
-  fmt.Println("component type", t,"component id", idx)
-  // fmt.Println("entity to data", )
-
-  value := w.componentMap[val].data.Index(idx)
-  out := value.Interface().(T)
-
-  return &out
-}
-
-func main() {
-	world := newWorld(10)
-	Position := makeWorldComponent[Pos](world)
-	CTransform := makeWorldComponent[Transform](world)
-	CPlayer := makeWorldComponent[Player](world)
-
-  playerSystem := PlayerSystem{}
-  id := world.registerSystem(&playerSystem)
-
-	// playerSystem := makeWorldSystem[PlayerSystem](world)
-  {
-    signature := NewSignature(MAX_SIGNATURE)
-    signature.Set(CTransform.Id())
-    signature.Set(CPlayer.Id())
-    world.setSystemSignature(id, signature)
-  }
-
-
-  player := world.newEntity(Transform{pos: rl.NewVector2(0, 0), vel: rl.NewVector2(5, 0)}, Player{})
-	world.newEntity(Pos{5, 10})
-  UNUSED(player, Position)
-	// world.newEntity(Pos{1000, 10001})
-
-
-
-
-  transform := getComponent[Transform](world, player)
-  fmt.Println("player pos?", transform)
-  // fmt.Println(world.elemToComponent)
-
-
-	UNUSED(fmt.Println, playerSystem)
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+func (e *Engine) run(scene IScene) {
+	rl.InitWindow(960, 640, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
-
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
-    world.run()
-		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+    // Update__________________________________________________________________
+		rl.BeginDrawing()
+    rl.ClearBackground(color.RGBA{R: 24, G: 24, B: 24})
+
+    rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+    rl.DrawText("hello", 32, 32, 20, rl.Black)
 
 		rl.EndDrawing()
 	}
 }
+
+func main() {
+  // var engine = Engine{}
+  // engine.run(&Game{})
+
+  var world = NewWorld()
+  world.registerComponent(&CTest{})
+  world.registerComponent(&CComp{})
+
+
+  var signature = NewSignature(MAX_SIGNATURE_SIZE)
+  signature.Set(world.getComponentId(&CComp{}))
+  world.registerSystem(&PlayerSystem{}, signature)
+
+  world.newEntity(&CTest{val: 100})
+
+  world.newEntity(&CComp{val: 100})
+  player := world.newEntity(&CComp{val: 100}, &CTest{val: 5})
+
+  var playerTest *CTest
+
+  world.getComponent(player, &playerTest)
+  fmt.Println("did i get it?", playerTest)
+  // fmt.Println(world.components[0].data.Index(0))
+}
+
+// ctest := newWorldComponent[CTest](world)
+// ctest.get()
+// ctest.set()
+// ctest.all()
+// player.get(CTest{})
+
+// Issues
+// current way of handling components is error prone. For example, we must pass
+// a reference to a component to every world function that handles components.
+// forgetting to add '&' will treat the component type as a different componentArray
+// Possible Solutions:
+// 1. Do it once. Users can create a type that will deal with correctly passing
+//    in the component structs.
+//    Position := newWorldType[Pos](world)
+//    Position.get(entity)
+//    Position.set(entity, Pos{10, 20})
+// 2. Make it so that users will receive an error, or the program panicking if
+//    the user doesn't pass in a reference to a component type.
+
+// a component should have its own store
+// a comonent should have an id
+// a user should be able to query the components id
+// a user should be able to create an entity
