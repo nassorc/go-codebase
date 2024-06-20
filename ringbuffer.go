f6305618443cf116f41574d0b558ca5fe1b4f77c
nassorc <macrossan17@gmail.com>
2024-06-19 22:04:02 -0700

    Add helper functions to inspect state of a ringbuffer

--- ringbuffer.go
+++ ringbuffer.go
@@ -1,45 +1,64 @@
 package gandalf
 
-import (
-	"fmt"
-	"reflect"
-)
-
 func NewRingBuffer[T any](capacity int) *Ringbuffer[T] {
 	return &Ringbuffer[T]{
 		read:     0,
 		write:    -1,
 		capacity: capacity,
-		buffer:   make([]T, capacity, capacity),
+		buffer:   make([]T, capacity),
 	}
 }
 
 type Ringbuffer[T any] struct {
 	read     int
 	write    int
+	size     int
 	capacity int
 	buffer   []T
 }
 
-func (buf *Ringbuffer[T]) Enqueue(value T) {
+func (buf *Ringbuffer[T]) Enqueue(value T) bool {
+	if buf.Full() {
+		return false
+	}
 	buf.write = (buf.write + 1) % buf.capacity
 	buf.buffer[buf.write] = value
+	buf.size += 1
 
-	if buf.read == buf.write && len(buf.buffer) > 0 {
+	if buf.read == buf.write && buf.size > 0 {
 		buf.read = (buf.read + 1) % buf.capacity
 	}
+	return true
 }
 
-func (buf *Ringbuffer[T]) Dequeue() (T, error) {
-	t := reflect.TypeOf((*T)(nil))
-	zero := reflect.Zero(t).Interface()
-
-	if len(buf.buffer) == 0 {
-		return zero.(T), fmt.Errorf("empty buffer")
+func (buf *Ringbuffer[T]) Deque() (T, bool) {
+	if buf.Empty() {
+		var zeroValue T
+		return zeroValue, false
 	}
 
 	out := buf.buffer[buf.read]
 	buf.read = (buf.read + 1) % buf.capacity
+	buf.size -= 1
 
-	return out, nil
+	return out, true
+}
+
+func (buf *Ringbuffer[T]) Size() int {
+	return buf.size
+}
+
+func (buf *Ringbuffer[T]) Full() bool {
+	return buf.size == buf.capacity
+}
+
+func (buf *Ringbuffer[T]) Empty() bool {
+	// initialized to -1
+	if buf.write == -1 {
+		return true
+	}
+	if buf.Size() == 0 {
+		return true
+	}
+	return false
 }
