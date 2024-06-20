cd7badcfd365a63f2335c99ed0997c6abc3ec936
nassorc <macrossan17@gmail.com>
2024-06-19 22:04:17 -0700

    Update ringbuffer tests

--- ringbuffer_test.go
+++ ringbuffer_test.go
@@ -4,37 +4,155 @@ import (
 	"testing"
 )
 
-func Test_RingBuffer(t *testing.T) {
-	rb := NewRingBuffer[int](3)
+func TestRingBuffer(t *testing.T) {
+	var buf = NewRingBuffer[int](3)
 
-	rb.Enqueue(10)
-	rb.Enqueue(20)
-	rb.Enqueue(30)
-
-	out, err := rb.Dequeue()
-
-	if err != nil {
-		t.Error(err)
+	if got := buf.Empty(); got != true {
+		t.Errorf("Expected=%v, Got=%v", true, got)
 	}
 
-	if out != 10 {
-		t.Fatalf("expected=%d, got=%d", 10, out)
+	tt := []int{3, 5, 8}
+
+	for _, t := range tt {
+		buf.Enqueue(t)
 	}
 
-	out, _ = rb.Dequeue()
-	if out != 20 {
-		t.Fatalf("expected=%d, got=%d", 20, out)
+	if got := buf.Size(); got != 3 {
+		t.Errorf("Expected=%v, Got=%v", 3, got)
 	}
 
-	rb.Enqueue(100)
+	size := buf.Size()
 
-	out, _ = rb.Dequeue()
-	if out != 30 {
-		t.Fatalf("expected=%d, got=%d", 30, out)
+	for idx := 0; idx < size; idx++ {
+		got, ok := buf.Deque()
+
+		if !ok {
+			t.Errorf("Expected=%v, Got=%v", true, ok)
+		}
+		if got != tt[idx] {
+			t.Errorf("Expected=%v, Got=%v", tt[idx], got)
+		}
 	}
 
-	out, _ = rb.Dequeue()
-	if out != 100 {
-		t.Fatalf("expected=%d, got=%d", 100, out)
+	if got := buf.Empty(); got != true {
+		t.Errorf("Expected=%v, Got=%v", true, got)
+	}
+}
+
+func TestRingBufferEnqueueDeque(t *testing.T) {
+	var buf = NewRingBuffer[int](5)
+
+	tt := []int{3, 5, 8, 13, 21}
+
+	for _, t := range tt {
+		buf.Enqueue(t)
+	}
+
+	if got := buf.Size(); got != 5 {
+		t.Errorf("Expected=%v, Got=%v", 3, got)
+	}
+
+	if got, _ := buf.Deque(); got != 3 {
+		t.Errorf("Expected=%v, Got=%v", 3, got)
+	}
+
+	if got, _ := buf.Deque(); got != 5 {
+		t.Errorf("Expected=%v, Got=%v", 5, got)
+	}
+
+	buf.Enqueue(100)
+
+	for _, expected := range []int{8, 13, 21, 100} {
+		got, ok := buf.Deque()
+
+		if !ok {
+			t.Errorf("Expected=%v, Got=%v", true, ok)
+		}
+
+		if got != expected {
+			t.Errorf("Expected=%v, Got=%v", expected, got)
+		}
+	}
+
+	if got := buf.Empty(); got != true {
+		t.Errorf("Expected=%v, Got=%v", true, got)
+	}
+}
+
+func TestRingBufferIsEmpty(t *testing.T) {
+	buf := NewRingBuffer[int](2)
+
+	if !buf.Empty() {
+		t.Errorf("expected ringbuffer to be empty.")
+	}
+
+	buf.Enqueue(3)
+	if buf.Empty() {
+		t.Errorf("expected ringbuffer to NOT be empty.")
+	}
+
+	buf.Enqueue(5)
+	if buf.Empty() {
+		t.Errorf("expected ringbuffer to NOT be empty.")
+	}
+
+	// test if enqueuing full buffer breaks code
+	buf.Enqueue(8)
+	if buf.Empty() {
+		t.Errorf("expected ringbuffer to NOT be empty.")
+	}
+
+	buf.Deque()
+	buf.Deque()
+	// should be empty
+	if !buf.Empty() {
+		t.Errorf("expected ringbuffer to be empty.")
+	}
+	buf.Deque()
+	if !buf.Empty() {
+		t.Errorf("expected ringbuffer to be empty.")
+	}
+}
+
+func TestRingBufferDequeReturnsFalseIfEmpty(t *testing.T) {
+	buf := NewRingBuffer[int](2)
+
+	if got := buf.Empty(); got != true {
+		t.Errorf("Expected=%v, Got=%v", true, got)
+	}
+
+	if _, ok := buf.Deque(); ok != false {
+		t.Errorf("Expected=%v, Got=%v", false, ok)
+	}
+
+	buf.Enqueue(2)
+	if _, ok := buf.Deque(); ok != true {
+		t.Errorf("Expected=%v, Got=%v", false, ok)
+	}
+	if _, ok := buf.Deque(); ok != false {
+		t.Errorf("Expected=%v, Got=%v", false, ok)
+	}
+}
+
+func BenchmarkRingBufferEnqueue(b *testing.B) {
+	size := b.N
+	buf := NewRingBuffer[int](size)
+
+	for idx := 0; idx < size; idx++ {
+		buf.Enqueue(idx)
+	}
+}
+
+func BenchmarkRingBufferDequeue(b *testing.B) {
+	size := b.N
+	buf := NewRingBuffer[int](size)
+
+	for idx := 0; idx < size; idx++ {
+		buf.Enqueue(idx)
+	}
+
+	b.ResetTimer()
+	for idx := 0; idx < size; idx++ {
+		buf.Deque()
 	}
 }
