6b008ff4bbf026ab6abcc30a55e5a0cd101cca60
nassorc <macrossan17@gmail.com>
2024-07-02 10:46:59 -0700

    Add type to manage animation state

--- asset.go
+++ asset.go
@@ -7,6 +7,32 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+type Animation struct {
+	texture     rl.Texture2D
+	row         int
+	col         int
+	offsetRow   int
+	offsetCol   int
+	totalFrames int
+	width       int
+	height      int
+	tileWidth   int
+	tileHeight  int
+	curFrame    int
+	speed       int
+	rotation    int
+}
+
+func (a *Animation) update(n int) {
+	// assumes that the animation is laid out from left to right
+	var curframe = (n / a.speed) % a.totalFrames
+	// if current frame exceeds texture's width, advance to next row
+	var nrow = (curframe * a.tileWidth) / int(a.texture.Width)
+	a.curFrame = curframe
+	a.row = (nrow + a.offsetRow) * a.tileHeight
+	a.col = ((curframe + a.offsetCol) * a.tileWidth) % int(a.texture.Width)
+}
+
 type AssetManager struct {
 	textures map[string]rl.Texture2D
 }
