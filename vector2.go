b66897c8e3611cb0d74cdfaca7430229cbc0e3f4
nassorc <macrossan17@gmail.com>
2024-07-18 05:22:29 -0700

    Implement function to subtract vectors and a vector contructor method

--- vector2.go
+++ vector2.go
@@ -1,10 +1,7 @@
 package gandalf
 
-type AABB struct {
-	X1 float32
-	Y1 float32
-	X2 float32
-	Y2 float32
+func NewVec2(x float32, y float32) Vec2 {
+	return Vec2{x, y}
 }
 
 type Vec2 struct {
@@ -19,9 +16,23 @@ func Vec2Add(v1, v2 Vec2) Vec2 {
 	}
 }
 
+func Vec2Subtract(v1, v2 Vec2) Vec2 {
+	return Vec2{
+		X: v1.X - v2.X,
+		Y: v1.Y - v2.Y,
+	}
+}
+
 func Vec2Scale(v Vec2, scale float32) Vec2 {
 	return Vec2{
 		X: v.X * scale,
 		Y: v.Y * scale,
 	}
+}
+
+type AABB struct {
+	X1 float32
+	Y1 float32
+	X2 float32
+	Y2 float32
 }
