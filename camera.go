ce9b30e9956bf93c9150ccd8ba553d026d96f31e
nassorc <macrossan17@gmail.com>
2024-08-05 07:20:45 -0700

    Update camera

--- camera.go
+++ camera.go
@@ -6,17 +6,30 @@ type Camera struct {
 	ViewPort AABB // X, Y, Width, Height
 	Position Vec2 // TOP-LEFT point
 	Zoom     float64
+  view     *ebiten.Image
 }
 
-func (c *Camera) Draw(world *ebiten.Image, screen *ebiten.Image) {
+func (c Camera) Draw(world *ebiten.Image, screen *ebiten.Image) {
 	if c.Zoom == 0 {
 		c.Zoom = 1
 	}
-	ops := &ebiten.DrawImageOptions{}
 
-	ops.GeoM.Translate(-float64(c.Position.X), -float64(c.Position.Y))
-	ops.GeoM.Scale(c.Zoom, c.Zoom)
-	screen.DrawImage(world, ops)
+	if c.view == nil {
+    c.view = ebiten.NewImage(int(c.ViewPort.X2), int(c.ViewPort.Y2))
+	}
+
+  c.view.Clear()
+
+	cameraOps := &ebiten.DrawImageOptions{}
+  // move camera in game world
+	cameraOps.GeoM.Translate(-float64(c.Position.X), -float64(c.Position.Y))
+	cameraOps.GeoM.Scale(c.Zoom, c.Zoom)
+
+	viewportOps := &ebiten.DrawImageOptions{}
+  viewportOps.GeoM.Translate(float64(c.ViewPort.X1), float64(c.ViewPort.Y1))
+
+  c.view.DrawImage(world, cameraOps)
+	screen.DrawImage(c.view, viewportOps)
 }
 
 func (c *Camera) Move(x float32, y float32) {
